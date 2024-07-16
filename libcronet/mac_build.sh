89732cff6e46f5aa67a8815be587e97ec121f372
chonglou <chonglou@minimaxi.com>
2024-07-16 16:49:27 +0800

    test macos

---
+++ libcronet/mac_build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# 获取当前工作目录的路径
+CURRENT_DIR="$(pwd)"
+
+# 设置脚本遇到错误即停止执行
+set -e
+
+# 在当前工作目录下创建一个新目录 'out'，并清空其中的内容
+mkdir -p out
+rm -rf out/*
+cd out
+
+# 克隆 depot_tools 仓库，将其路径加入到环境变量中
+git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH="${CURRENT_DIR}/out/depot_tools:$PATH"
+# 克隆 Chromium 源码仓库的指定分支
+git clone -b "${CHROMIUM_VERSION}" --depth=2 https://chromium.googlesource.com/chromium/src
+
+# 进入到源码目录，应用 patch
+cd src
+git apply ../../libcronet/proxy_support_*.patch
+cd ..
+echo 'solutions = [
+      {
+        "name": "src",
+        "url": "https://chromium.googlesource.com/chromium/src.git",
+        "managed": False,
+        "custom_deps": {},
+        "custom_vars": {},
+      },
+]' > .gclient
+
+# 同步源码，跳过历史记录和钩子
+gclient sync --no-history --nohooks
+
+# 安装 psutil 依赖
+pip install psutil
+
+# 运行 gclient 钩子
+gclient runhooks
+
+cd src/components/cronet
+
+gn gen out/Cronet/ --args='target_os="mac" is_debug=false is_component_build=false target_cpu="arm64"'
+
+ninja -C out/Cronet cronet_package
+
+cp -r out/Cronet/cronet/ "${CURRENT_DIR}/cronet_build"
+
+cd "${CURRENT_DIR}"
+rm -rf out
---
+++ libcronet/mac_build_wheels.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# 获取当前工作目录的路径
+CURRENT_DIR="$(pwd)"
+
+# 设置脚本遇到错误即停止执行
+set -e
+
+export CPPFLAGS="-I${CURRENT_DIR}/cronet_build/include"
+export LDFLAGS="-L${CURRENT_DIR}/cronet_build"
+
+# https://github.com/pypa/wheel/issues/406
+export _PYTHON_HOST_PLATFORM="macosx-11.0-arm64"
+export ARCHFLAGS="-arch arm64"
+
+function repair_wheel() {
+  local python_version="$1"
+  local wheel_version="$2"
+
+  file $(which python)
+
+  arch -arm64 python -m pip install delocate build
+  arch -arm64 python -m build
+  ls "${CURRENT_DIR}"
+  delocate-wheel -w wheelhouse -v "dist/python_cronet-${LIBCRONET_VERSION}-cp${wheel_version}-cp${wheel_version}-macosx_11_0_arm64.whl"
+}
+
+# 将 matrix.python-version 传递到脚本中，并生成 wheel
+PYTHON_VERSION=$1
+
+# 映射 Python 版本到 wheel 版本
+case $PYTHON_VERSION in
+  "3.8") WHEEL_VERSION="38" ;;
+  "3.9") WHEEL_VERSION="39" ;;
+  "3.10") WHEEL_VERSION="310" ;;
+  "3.11") WHEEL_VERSION="311" ;;
+  "3.12") WHEEL_VERSION="312" ;;
+  *) echo "Unsupported Python version: $PYTHON_VERSION" ; exit 1 ;;
+esac
+
+repair_wheel "${PYTHON_VERSION}" "${WHEEL_VERSION}"
