a2d6d25783a5f5301bab52a4fd1ec814bb3532de
chonglou <chonglou@minimaxi.com>
2024-07-17 14:39:05 +0800

    test macos

--- libcronet/mac_build_wheels.sh
+++ libcronet/mac_build_wheels.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
 echo "Building with Chromium version: ${CHROMIUM_VERSION}"
 echo "Using libcronet version: ${LIBCRONET_VERSION}"
-
 
 # 获取当前工作目录的路径
 CURRENT_DIR="$(pwd)"
@@ -14,7 +12,8 @@ set -e
 ## Fix directory structure
 sudo mkdir -p /usr/local/lib && \
     sudo chown -R $(whoami) /usr/local/lib
-
+# copy libcronet
+sudo cp -v ${CURRENT_DIR}/cronet_build/libcronet*dylib /usr/local/lib
 
 export CPPFLAGS="-I${CURRENT_DIR}/cronet_build/include"
 export LDFLAGS="-L${CURRENT_DIR}/cronet_build"
@@ -30,15 +29,11 @@ function repair_wheel() {
 
   file $(which python${python_version})
 
-  arch -arm64 python${python_version} -m pip install delocate build
-  arch -arm64 python${python_version} -m build
-
-  cp -v ${CURRENT_DIR}/cronet_build/libcronet*dylib /usr/local/lib
-
-  delocate-wheel -w wheelhouse -v dist/python_cronet-${LIBCRONET_VERSION}-cp${wheel_version}-cp${wheel_version}-macosx_11_0_arm64.whl
+#  arch -arm64 python${python_version} -m pip install delocate build
+#  arch -arm64 python${python_version} -m build
+#
+#  delocate-wheel -w wheelhouse -v dist/python_cronet-${LIBCRONET_VERSION}-cp${wheel_version}-cp${wheel_version}-macosx_11_0_arm64.whl
 }
-
-
 
 repair_wheel "3.8" "38"
 repair_wheel "3.9" "39"
